7c834909d8f62043df7f73434c1c94af5fac1a7a
Pavel-Gornov <gornovpavel30@yandex.ru>
2025-10-18 19:34:58 +0400

    2-e задания варианта

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "LinkedList.h"
 #include <random>
 
+typedef unsigned short digit;
+
 struct Lodger {
     char name[64];
     size_t age;
@@ -35,6 +37,85 @@ void task_9(size_t n) {
     }
 }
 
+LinkedList<digit> int_to_list(int number) {
+    LinkedList<digit> list = LinkedList<digit>();
+    while (number) {
+        list.push_head(number % 10);
+        number /= 10;
+    }
+    return list;
+}
+
+LinkedList<digit> task_2_sum(const LinkedList<digit>& num1, const LinkedList<digit>& num2) {
+    LinkedList<digit> res = LinkedList<digit>();
+    digit remainder = 0;
+    digit sum = 0;
+    if (num1.size() > num2.size()) {
+        for(size_t i = 0; i < num1.size(); i++) {
+            if (i < num2.size())
+                sum = num1[num1.size() - i - 1] + num2[num2.size() - i - 1] + remainder;
+            else
+                sum = num1[num1.size() - i - 1] + remainder;
+
+            if (sum > 9) {
+                remainder = 1;
+                sum -= 10;
+            }
+            else 
+                remainder = 0;
+            res.push_head(sum);
+        }
+    }
+    else {
+        for(size_t i = 0; i < num2.size(); i++) {
+            if (i < num1.size())
+                sum = num2[num2.size() - i - 1] + num1[num1.size() - i - 1] + remainder;
+            else
+                sum = num2[num2.size() - i - 1] + remainder;
+
+            if (sum > 9) {
+                remainder = 1;
+                sum -= 10;
+            }
+            else 
+                remainder = 0;
+            res.push_head(sum);
+        }
+    }
+    if (remainder)
+        res.push_head(remainder);
+    return res;
+}
+
+LinkedList<digit> task_2_mult(const LinkedList<digit>& num1, const LinkedList<digit>& num2) {
+    LinkedList<digit> res = LinkedList<digit>();
+    digit mult = 0;
+    digit remainder = 0;
+    digit prod = 0;
+    for (size_t i = 0; i < num1.size(); i++) {
+        mult = num1[num1.size() - i - 1];
+        LinkedList<digit> sum = LinkedList<digit>();
+        for (size_t k = 0; k < i; k++)
+            sum.push_tail(0);
+
+        for (size_t j = 0; j < num2.size(); j++) {
+            prod = mult * num2[num2.size() - j - 1] + remainder;
+            if (prod > 9) {
+                remainder = prod / 10;
+                prod = prod % 10;
+            }
+            else 
+                remainder = 0;
+            sum.push_head(prod);
+        }
+        if (remainder) {
+            sum.push_head(remainder);
+            remainder = 0;
+        }
+        res = task_2_sum(res, sum);
+    }
+    return res;
+}
 
 int main() {
     #ifdef _WIN32
@@ -42,5 +123,9 @@ int main() {
     #endif
     std::ios::sync_with_stdio(false);
 
-    task_9(25);
+    LinkedList<digit> number1 = int_to_list(INT_MAX);
+    LinkedList<digit> number2 = int_to_list(INT_MAX);
+    std::cout << task_2_mult(number1, number2) << "\n";
+
+    // task_9(25);
 }
