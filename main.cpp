0f50d395d94c76bfd738f9a4823c75c9519f1d71
Pavel-Gornov <gornovpavel30@yandex.ru>
2025-10-18 11:32:40 +0400

    Полная реализация списка

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 
 int main() {
     LinkedList<int> list = LinkedList<int>();
+    LinkedList<int> list2 = LinkedList<int>();
+    list2.push_head(-1);
+
     list.push_head(1);
     list.push_head(2);
     list.push_head(3);
+    list.push_tail(0);
+    list.push_head(4);
+    list.push_head(4);
+    list.push_tail(4);
+    list2 = list;
+
+    std::cout << list << '(' << list.size() << ")\n";
+    list2.delete_node(4);
+    std::cout << list2 << '(' << list2.size() << ")\n";
 }
